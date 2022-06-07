a63c1e6d9b536829e409951e841290835bb6761e
idaism <ida.bjarney@outlook.com>
2022-06-07 12:30:34 +0100

    finished lesson 84

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El =  document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,4 +18,35 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
 // Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // 2. Display the dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for rolled 1:
+  if (dice !== 1) {
+    // Add number to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  } else {
+    // Switch to next player
+    
+    // Resets current score of the player that just finished their round
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    // If active player is 0, switch to 1/ if 1 then switch to 0
+    activePlayer = activePlayer === 0 
+    ? 1 
+    : 0; 
+    // Toggles the active class on the player background so it will stitch off on whichever is active and switch on the inactive one
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
